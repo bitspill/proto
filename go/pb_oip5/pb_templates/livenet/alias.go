89b3ef9262b161487730d9a8e015d3d06d7b4dc4
bitspill <git@bitspill.net>
2021-05-22 06:02:53 -0500

    SimpleAssetHeld

--- go/pb_oip5/pb_templates/livenet/alias.go
+++ go/pb_oip5/pb_templates/livenet/alias.go
@@ -6,6 +6,7 @@ type RegisteredPublisher = pb_templates.Tmpl_433C2783
 
 type Coin = pb_templates.Tmpl_29F96711
 type SimpleCoinSale = pb_templates.Tmpl_DE84D583
+type SimpleAssetHeld = pb_templates.Tmpl_EE0D326B
 type CommercialContent = pb_templates.Tmpl_D8D0F22C
 
 type Song = pb_templates.Tmpl_2670B072
